672f745466c39a15ec33ccca513ad705bf940b3c
Robin Tram <robin.tram@gmail.com>
2018-10-22 16:33:41 -0400

    added app and database folder

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,7 @@
 require_relative '../config/environment'
-
+require_relative '../app/models/drivers.rb'
+require_relative '../app/models/races.rb'
+require_relative '../app/models/standings.rb'
 
 
 puts "HELLO WORLD"
