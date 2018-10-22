0f617d92e245c4ebc43d58c543033ca7055aefdf
Robin Tram <robin.tram@gmail.com>
2018-10-22 16:49:28 -0400

    created standings table

---
+++ db/migrate/20181022204721_create_standings_table.rb
@@ -0,0 +1,10 @@
+class CreateStandingsTable < ActiveRecord::Migration[5.0]
+  def change
+    create_table :standings do |t|
+      t.integer :driver_id
+      t.integer :race_id
+      t.integer :points
+      t.boolean :wins
+    end 
+  end
+end
