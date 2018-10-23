3a9613acb406cb7bb78bbde0f6e084b00bae906e
Robin Tram <robin.tram@gmail.com>
2018-10-23 16:57:05 -0400

    driver search in CLI

--- bin/run.rb
+++ bin/run.rb
@@ -1,14 +1,72 @@
 require_relative '../config/environment'
 
 
+def greeting
+puts "Welcome to the Formula 1 Database!"
+puts ""
+puts "Your number one source for Formula 1!"
+puts ""
+end
 
-puts "HELLO WORLD"
+def category_prompt
+puts "Are you looking for more information about a F1 Driver or a Circuit?"
+end
 
-lewis = Driver.find(1)
-heidfeld = Driver.find(2)
-vettel = Driver.find(20)
+def user_input
+  gets.chomp
+end
 
-agp = Race.find(1)
-usgp = Race.find(62)
+def run_driver(driver)
+  puts "Driver Commands"
+  puts ""
+end
+
+def run_race
+  puts "Race Commands"
+end
+
+def driver_or_circuits(input)
+  if ["F1 Driver", "driver", "DRIVER", "drivers", "DRIVERS"].include? (input)
+    puts "Please enter a driver name."
+    input = gets.chomp
+    driver_search(input)
+    # run_driver
+  elsif ["Circuit", "circuit", "CIRCUIT", "Circuits", "circuits"].include? (input)
+    puts "Please enter a circuit name."
+    input = gets.chomp
+    circuit_search(input)
+    # run_race
+  end
+end
+
+def driver_search(input)
+  driver = Driver.select("id").where(["first_name LIKE ? OR last_name LIKE ?", "%#{input}%", "%#{input}%"])
+  if driver.length == 1
+    driver_obj = Driver.find(driver[0].id)
+    puts "#{driver_obj.full_name} has been found!"
+    driver_obj
+  end
+  # binding.pry
+end
+
+def circuit_search(input)
+end
+
+def runner
+greeting
+
+category_prompt
+
+input = user_input
+
+driver_or_circuits(input)
+
+
+end
+
+
+
+
+
 binding.pry
 0
