723563f123aa3362e50ae9e339157d64a1d2316b
Robin Tram <robin.tram@gmail.com>
2018-10-23 11:51:54 -0400

    11:45 am

--- app/models/races.rb
+++ app/models/races.rb
@@ -1,4 +1,5 @@
 class Race < ActiveRecord::Base
   has_many :standings
   has_many :drivers, through: :standings
+
 end
--- bin/run.rb
+++ bin/run.rb
@@ -7,5 +7,7 @@ puts "HELLO WORLD"
 lewis = Driver.find(1)
 heidfeld = Driver.find(2)
 vettel = Driver.find(20)
+
+agp = Race.find(1)
 binding.pry
 0
